d1ddcbc8ee01ab56df23bb77f07cc93aa2dcc55c
pranotobudi <pranotobudi.s@gmail.com>
2019-03-25 06:50:26 +0700

    create custom User, custom UserManager, custom UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email','password']
+    list_filter = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Register your models here.
--- users/models.py
+++ users/models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    #to override custom create_user and custom create_superuser related with EMAIL
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email, and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email, and password.
+        """
+        user = self.create_user(
+            email,
+            password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    # add additional fields in here
+    email =  models.CharField(max_length=120, unique=True)
+    password =  models.CharField(max_length=120, default='')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager() #to have custom create_user and custom create_superuser
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
+
+    def __str__(self):
+        return (f"{self.id} {self.email}")
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+    
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
 
 # Create your models here.
